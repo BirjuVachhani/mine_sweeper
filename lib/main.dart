7248a37f403d86372421e657539768a7bfc462c4
BirjuVachhani <brvachhani@gmail.com>
2025-09-02 17:35:52 +0530

    Change seedColor to yellow

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,14 @@ class MinesweeperApp extends StatelessWidget {
       title: 'Minesweeper',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+          seedColor: Colors.yellow,
           brightness: Brightness.light,
         ),
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+          seedColor: Colors.yellow,
           brightness: Brightness.dark,
         ),
         useMaterial3: true,
