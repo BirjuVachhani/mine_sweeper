1dc1faa15350253fc44659a4a161fabe68454353
BirjuVachhani <brvachhani@gmail.com>
2025-09-02 17:36:32 +0530

    Rollback to: Change the seedColor to indigo

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,3 @@ class MinesweeperApp extends StatelessWidget {
   const MinesweeperApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minesweeper',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.indigo,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.indigo,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-      themeMode: ThemeMode.system,
-      home: const GameScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
