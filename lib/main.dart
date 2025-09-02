c5a4bf14c5c5cfd17141f0a869f7784694c500de
BirjuVachhani <brvachhani@gmail.com>
2025-09-02 17:24:14 +0530

    For the code present, we get these errors: ``` error • lib/main.dart:16:22 • Expected to find ';'. null • expected_token ``` ``` error • lib/main....

--- lib/main.dart
+++ lib/main.dart
@@ -13,4 +13,22 @@ class MinesweeperApp extends StatelessWidget {
     return MaterialApp(
       title: 'Minesweeper',
       theme: ThemeData(
-        colorScheme: Color
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const GameScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
