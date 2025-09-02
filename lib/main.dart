a7a55530a103f1dfa989744bb4b8d8d2c25eb8ae
BirjuVachhani <brvachhani@gmail.com>
2025-09-02 17:23:22 +0530

    Rollback to: Change color scheme seed color to purple.

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,4 @@ class MinesweeperApp extends StatelessWidget {
     return MaterialApp(
       title: 'Minesweeper',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-      themeMode: ThemeMode.system,
-      home: const GameScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+        colorScheme: Color
