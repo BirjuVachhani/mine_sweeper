635586a632929fd7d586252adaf74319fefdd98a
BirjuVachhani <brvachhani@gmail.com>
2025-09-03 13:33:46 +0530

    Change seel color to orange

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,14 @@ class MinesweeperApp extends StatelessWidget {
       title: 'Minesweeper',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
+          seedColor: Colors.orange,
           brightness: Brightness.light,
         ),
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
+          seedColor: Colors.orange,
           brightness: Brightness.dark,
         ),
         useMaterial3: true,
