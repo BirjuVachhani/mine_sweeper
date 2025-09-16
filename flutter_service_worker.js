0659d14d03b1aa92df5d76844f24fcfa378e1ee5
BirjuVachhani <20423471+BirjuVachhani@users.noreply.github.com>
2025-09-16 15:40:17 +0530

    New Deployment: 2025-09-16T10:10:17.635244Z

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0374893ebf4fe5c5b6714cf74cc00176",
+const RESOURCES = {"flutter_bootstrap.js": "9e93f09013fc32909b8a762e1ad14f6d",
 "version.json": "f40bdd70ee38eca3612d271fa955e5a1",
-"index.html": "0d4d8e159ebbfec157a8526a41ea6699",
-"/": "0d4d8e159ebbfec157a8526a41ea6699",
+"index.html": "7f285647d2b1d72b7c45a964fd1f9c0c",
+"/": "7f285647d2b1d72b7c45a964fd1f9c0c",
 "main.dart.js": "b1f1a2085df9508c430721b941bf540e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
