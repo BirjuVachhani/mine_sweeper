d95cb6448c67e363a36379321ebc9f2256a14ce5
BirjuVachhani <20423471+BirjuVachhani@users.noreply.github.com>
2025-09-22 15:02:02 +0000

    Change the seed color to green

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MinesweeperApp extends StatelessWidget {
       title: 'Minesweeper',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.red,
+          seedColor: Colors.green,
           brightness: Brightness.light,
         ),
         useMaterial3: true,
